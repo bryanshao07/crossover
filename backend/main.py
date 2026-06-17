4f06cea1c98b4e7e2065bfcd3c4b243c1f8628b7
Bryan Shao <bs887@cornell.edu>
2026-06-17 16:57:48 -0700

    feat(backend): universe router

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 import data_store
 from config import settings
-from routers import compare, players
+from routers import compare, players, universe
 from services import similarity
 
 data_store.load()
@@ -21,3 +21,4 @@ app.add_middleware(
 
 app.include_router(players.router)
 app.include_router(compare.router)
+app.include_router(universe.router)
